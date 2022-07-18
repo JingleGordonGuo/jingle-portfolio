d5a289672e02555aa7efb3af4b842ec05ea9733a
JingleGordonGuo <jinglegordonguo@gmail.com>
2022-07-18 19:21:57 +1000

    Switched to using styled components global style

--- styles/index.ts
+++ styles/index.ts
@@ -1,4 +1,42 @@
-import { css } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+body {
+  position: fixed;
+  height: 100%;
+  overflow: scroll;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+button {
+  border: unset;
+  background-color: unset;
+  cursor: pointer;
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
+`;
 
 export const flexCenter = css`
   display: flex;
