ac10fadc94c35c26f1d1d10eb88ac1332e8da3d3
JingleGordonGuo <jinglegordonguo@gmail.com>
2022-07-27 18:51:13 +1000

    Added croissant page

---
+++ pages/Croissant.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const CroissantDiv = styled.div``;
+
+const Croissant = () => {
+  return (
+    <CroissantDiv>
+      <h1>This is where I would put my croissant...</h1>
+      <h1>IF I HAD ANY!!!</h1>
+    </CroissantDiv>
+  );
+};
+
+export default Croissant;
